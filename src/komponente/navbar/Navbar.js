dbc2988e087312c5cba987355546aae57a1d6167
Ivan Francic <ivfrancic@gmail.com>
2021-02-24 10:46:11 +0100

    zavrsen navbar

--- src/komponente/navbar/Navbar.js
+++ src/komponente/navbar/Navbar.js
@@ -2,9 +2,21 @@ import React from 'react'
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <nav className='menu-area'>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Page 1</a></li>
+                <li><a href="/" className='logo'>Logo</a></li>
+                <li><a href="/">Page2</a></li>
+                <li><a  href="/">Dropdown ▾</a>
+                    <ul className='dropdown'>
+                        <li><a href="/">prvi</a></li>
+                        <li><a href="/">drugi</a></li>
+                        <li><a href="/">treci</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
